b8344d061191aa839483d55e5b8de6dcd1e3ff5d
NightMare <rahmanishfaq146@gmail.com>
2024-11-05 23:53:57 +0500

    my first next todo app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,187 @@
+'use client'
 import Image from "next/image";
+import { IoMdTrash } from "react-icons/io";
+import { MdModeEdit } from "react-icons/md";
+import './todo.css'
+import { useEffect, useState } from "react";
 
+type Todo = {
+  id:number,
+  title: string;
+  message: string;
+  checked: boolean;
+};
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todoData,setTodoData] = useState<Omit<Todo,'id'>>({
+    title:"",
+    message:"",
+    checked:false,
+  })
+  const [task,setTask] = useState<Todo[]>([])
+  const [taskId,setTaskId] = useState<number>(0)
+  const [filterTask, setFilterTask] = useState<string>('all');
+  const [searchedTask,setSearchedTask] = useState<string>('')
+  const [isEditing,setIsEditing] = useState<boolean>(false)
+  useEffect(() => {
+    const storedTask = localStorage.getItem('tasks')
+    if(storedTask){
+      setTask(JSON.parse(storedTask))
+    }
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('tasks',JSON.stringify(task))
+    
+  }, [task])
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>)=>{
+    const {name,value} = e.target
+    setTodoData({
+      ...todoData,
+      [name]:value,
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    })
+  }
+  const addTodo = ()=>{
+    if(todoData.title==='' || todoData.message===''){
+      alert('Please enter title or message')
+    }else if(todoData.title==='' || todoData.message==='' || isEditing){
+      setTask((prevTask)=>(
+        prevTask.map((sTask)=>sTask.id===taskId?{...sTask,title:todoData.title,message:todoData.message,checked:false}:sTask)
+      ))
+      setIsEditing(false)
+      setTodoData({...todoData,title:'',message:''})
+    }
+    else{
+      const newTodo = {...todoData,id:Date.now()}
+      setTask([...task,newTodo])
+      setTodoData({...todoData,title:'',message:''})
+    }
+  }
+  const toggleChecked = (id:number)=>{
+    setTask((prevTask)=>(
+      prevTask.map((task)=>task.id===id ?{...task,checked:!task.checked}:task)
+    ))
+  }
+  const deleteTask = (id:number)=>{
+    setTask((prevTask)=>(
+      prevTask.filter((task)=>task.id!==id)
+    ))
+  }
+  const editTask =(id:number)=>{
+    const editThisTask = task.find((sTask)=>sTask.id===id)
+    if(editThisTask){
+      setTaskId(id)
+     setIsEditing(true)
+     setTodoData({title:editThisTask.title,message:editThisTask.message,checked:editThisTask.checked})
+    }   
+}
+const filteredTasks = task.filter((todo) => {
+  if (filterTask === 'completed') return todo.checked;
+  if (filterTask === 'incompleted') return !todo.checked;
+  return true; // 'all' option
+});
+
+const searchedTasks = filteredTasks.filter((todo) => {
+  return (
+    todo.title.toLowerCase().includes(searchedTask.toLowerCase()) 
+    // || todo.message.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+});
+
+  return (
+    <div className=" flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)] ">
+      <div className="mt-10 w-full max-w-sm">
+      <h1 className="text-center text-4xl text-[#5E6072] font-extrabold">Todo List</h1>
+      <div className="bg-[#EBECF4] p-4 flex flex-col space-y-4 mt-4">
+        <input 
+        className="p-3" 
+        name="title"
+        type="text" 
+        value={todoData.title}
+        placeholder="Type Title..."
+        onChange={handleChange}
+        />
+        <input 
+        className="p-3" 
+        name="message"
+        type="text" 
+        value={todoData.message}
+        placeholder="Type Message..."
+        onChange={handleChange}
+        />
+        <button 
+        onClick={addTodo}
+        className="bg-[#616FEE] p-2 text-white rounded-md"
+        >{isEditing?'Edit Task':'Add Task'}</button>
+      </div>
+      </div>
+      <div className=" mt-4 w-full max-w-[700px]">
+        
+        {task.length===0 ?
+        <>
+        <h1 className="text-[#5E6072]">
+          No Task added yet...
+        </h1>
+        </>:
+        <>
+        <div className="bg-[#ECEDF6] p-4 space-y-3">
+          {/* all todo goes here  */}
+          <div className="flex justify-between">
+            <input 
+            className="p-3"
+            type="text" 
+            value={searchedTask}
+            onChange={(e)=>setSearchedTask(e.target.value)}
+            placeholder="search here.."
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <select
+          value={filterTask}
+          onChange={(e)=>setFilterTask(e.target.value)}
+           className="p-2 bg-[#CCCEDF] rounded-md text-[#5A5A5A]"
+           >
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="incompleted">Incompleted</option>
+          </select>
+
+          </div>
+          {searchedTasks.map((sTask)=>{
+            const {id,title,message,checked} = sTask
+            return(
+              <div key={id} className="bg-white rounded-md p-4 flex justify-between">
+
+            <div className="flex space-x-4 justify-center items-center">
+            <input 
+            type="checkbox"
+            name="checked"
+            // checked = {true}
+            onChange={()=>toggleChecked(id)}
+             className="w-6 h-6 appearance-none border-2 border-gray-300 rounded-md checked:bg-[#616FEE] checked:border-transparent focus:outline-none focus:ring-2 focus:ring-[#616FEE] cursor-pointer relative"
+             />
+            <div>
+            <p className={checked?'text-lg line-through':'text-lg'}>{title}</p>
+            <p className={checked?'text-sm line-through':'text-sm'}>{message}</p>
+            </div>
+            </div>
+
+            <div className="flex space-x-3">
+            <div 
+            onClick={()=>deleteTask(id)}
+            className="bg-[#ECEDF6] h-8 flex justify-center items-center p-1 cursor-pointer"><IoMdTrash  size={20} color="#5A5A5A"/>
+            </div>
+            <div 
+            onClick={()=>editTask(id)}
+            className="bg-[#ECEDF6] h-8 flex justify-center items-center p-1 cursor-pointer">
+              <MdModeEdit size={20} color="#5A5A5A"/>
+            </div>
+            </div>
+          </div>
+            )
+          })}
+
+        </div> 
+        </>}
+      </div>
     </div>
   );
 }
